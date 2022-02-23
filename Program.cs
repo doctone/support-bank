40f777f76a2b0111ea27c148362bfd9277c87881
Sam James <samjojames@gmail.com>
2022-02-23 10:49:54 +0000

    Include obj folder in gitignore

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace SupportBank
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Support Bank!");
+        }
+    }
+}
