c84f450f9bcdf9545e3e33745ea94616a74818e1
Sam James <samjojames@gmail.com>
2022-02-24 12:43:13 +0000

    Removed transaction storing on each Account class

--- Account.cs
+++ Account.cs
@@ -3,11 +3,10 @@ namespace SupportBank
     public class Account
         {
             public string AccountName { get; set; }
-            public List<Transaction> Transactions { get; set; }
+
             public Account(string accountName)
             {
                 AccountName = accountName;
-                Transactions = new List<Transaction>();
             }
             
         }
