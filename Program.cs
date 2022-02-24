08da223082748bacaa71d23d6553c6490d8d0713
Sam James <samjojames@gmail.com>
2022-02-24 12:17:00 +0000

    Remmove readline Function

--- Program.cs
+++ Program.cs
@@ -12,32 +12,8 @@ namespace SupportBank
           var accounts = newBank.GetAccounts();
         //   newBank.PrintTransactions();
           
-          newBank.ListAll();
+          // newBank.ListAll();
           newBank.PrintTransactions("Ben B");
-
-        }
-        public static void List(SupportBank bank)
-        {
-          Console.WriteLine("1. List All");
-          Console.WriteLine("2. List Account");
-
-          var input = Console.ReadLine();
-
-          if ( input == "1" )
-          {
-            bank.ListAll();
-          } else if 
-          ( input == "2" )
-          {
-            Console.WriteLine("Which account would you like to view?");
-            var accounts = bank.GetAccounts();
-            foreach (Account account in accounts)
-            {
-              Console.WriteLine(account.AccountName);
-            }
-            var nameChoice = Console.ReadLine();
-            // bank.ListAccount(nameChoice)
-          }
         }
     }
 }
