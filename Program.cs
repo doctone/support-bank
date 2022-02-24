ff40a6230dbb798953e8227f794a5497a9b980fb
Sam James <samjojames@gmail.com>
2022-02-24 10:30:50 +0000

    Make print function in main

--- Program.cs
+++ Program.cs
@@ -10,11 +10,33 @@ namespace SupportBank
           List<Transaction> transactions = fileReader.GetTransactions("./Transactions2014.txt");
           var newBank = new SupportBank(transactions, new List<Account>(){});
           var accounts = newBank.GetAccounts();
-        //   newBank.PrintTransactions();
-          
-          // newBank.ListAll();
           newBank.ListAccount(accounts[0]);
+
+          List(newBank);
+
         }
-      
+        public static void List(SupportBank bank)
+        {
+          Console.WriteLine("1. List All");
+          Console.WriteLine("2. List Account");
+
+          var input = Console.ReadLine();
+
+          if ( input == "1" )
+          {
+            bank.ListAll();
+          } else if 
+          ( input == "2" )
+          {
+            Console.WriteLine("Which account would you like to view?");
+            var accounts = bank.GetAccounts();
+            foreach (Account account in accounts)
+            {
+              Console.WriteLine(account.AccountName);
+            }
+            var nameChoice = Console.ReadLine();
+            // bank.ListAccount(nameChoice)
+          }
+        }
     }
 }
